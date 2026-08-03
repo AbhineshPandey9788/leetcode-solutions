e1bec1fd569cf9a24444a186de41da2d1dbae2e9
AbhineshPandey9788 <pandeyabhinesh194@gmail.com>
2026-08-04 01:38:53 +0530

    Sliding Window Maximum Solution

---
+++ Sliding Window/0239_Sliding_Window_Max.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        vector<int>ans;
+        deque<int>dq;   //stores indices
+
+        for(int i=0; i<nums.size(); i++){
+
+            while(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
+            }
+
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);  //stores index
+
+            if(i>=k-1){
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
